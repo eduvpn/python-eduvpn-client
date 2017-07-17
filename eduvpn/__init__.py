2b18ab6427d017669462d54630db78b1a49ba700
Gijs Molenaar <gijs@pythonic.nl>
2017-07-17 14:21:57 +0300

    print url

--- eduvpn/__init__.py
+++ eduvpn/__init__.py
@@ -157,7 +157,7 @@ def get_auth_url(oauth, code_verifier, auth_endpoint):
 
 
 def get_oauth_token_code(auth_url, port):
-    logger.info("Opening default webbrowser with authorization URL and waiting for callback on port {}".format(port))
+    logger.info("Opening default webbrowser with {} and waiting for callback on port {}".format(auth_url, port))
     webbrowser.open(auth_url)
     response = one_request(port)
     code = response['code'][0]
