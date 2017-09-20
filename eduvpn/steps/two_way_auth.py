e3ade488ebe8788a108c557266616352ff8cf544
Gijs Molenaar <gijs@pythonic.nl>
2017-09-20 23:59:22 +0200

    fix #69

--- eduvpn/steps/two_way_auth.py
+++ eduvpn/steps/two_way_auth.py
@@ -8,20 +8,27 @@ from eduvpn.steps.finalize import finalizing_step
 logger = logging.getLogger(__name__)
 
 
-def update(options, meta, dialog, oauth, two_dialog, builder):
+def update(options, meta, oauth, builder):
+    two_dialog = builder.get_object('2fa-dialog')
+
     for i, option in enumerate(options):
-        dialog.add_button(option, i)
+        two_dialog.add_button(option, i)
     two_dialog.show()
-    index = int(dialog.run())
+    index = int(two_dialog.run())
     if index >= 0:
         meta.username = options[index]
         logger.info("user selected '{}'".format(meta.username))
         finalizing_step(oauth=oauth, meta=meta, builder=builder)
-    dialog.destroy()
+    two_dialog.destroy()
 
 
-def background(meta, dialog, oauth, two_dialog, builder):
-    info = user_info(oauth, meta.api_base_uri)
+def background(meta, oauth, builder):
+    try:
+        info = user_info(oauth, meta.api_base_uri)
+    except Exception as e:
+        GLib.idle_add(lambda: error_helper(window, "Can't fetch user info", str(e)))
+        raise
+
     username = None
     if info['is_disabled']:
         window = builder.get_object('eduvpn-window')
@@ -30,18 +37,16 @@ def background(meta, dialog, oauth, two_dialog, builder):
     if 'two_factor_enrolled_with' in info:
         options = info['two_factor_enrolled_with']
         if len(options) > 1:
-            GLib.idle_add(lambda: update(options=options, meta=meta, dialog=dialog, oauth=oauth,
-                                         two_dialog=two_dialog, builder=builder))
+            GLib.idle_add(lambda: update(options=options, meta=meta, oauth=oauth, builder=builder))
             return
         elif len(options) == 1:
             meta.username = options[0]
-            logger.info("auto selected username {} for 2 factor authentication".format(username))
+            GLib.idle_add(lambda: error_helper(window, "two_factor_enrolled_with' doesn't contain any fields", ""))
+        else:
+            GLib.idle_add(lambda: error_helper(window, "This account has been disabled", ""))
         GLib.idle_add(lambda: finalizing_step(oauth=oauth, meta=meta, builder=builder))
 
 
 def two_auth_step(builder, oauth, meta):
     """checks if 2auth is enabled. If more than 1 option presents user with choice"""
-    dialog = builder.get_object('2fa-dialog')
-    two_dialog = builder.get_object('2fa-dialog')
-
-    thread_helper(lambda: background(meta, dialog, oauth, two_dialog, builder))
+    thread_helper(lambda: background(meta, oauth, builder))
