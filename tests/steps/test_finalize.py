5c6c408b6dfe7addfbb3b076fad0856f4a25559e
Gijs Molenaar <gijs@pythonic.nl>
2018-05-16 17:08:03 +0200

    better fix for fix failing test for ubuntu 18.04

--- tests/steps/test_finalize.py
+++ tests/steps/test_finalize.py
@@ -3,6 +3,10 @@ from mock import patch
 from tests.util import MockBuilder, MockOAuth, MockDialog
 from eduvpn.metadata import Metadata
 from eduvpn.steps.finalize import finalizing_step, _background
+from eduvpn.util import have_dbus
+
+if have_dbus():
+    from dbus.exceptions import DBusException
 
 
 class TestFinalize(TestCase):
@@ -23,7 +27,14 @@ class TestFinalize(TestCase):
     def test_finalizing_step(self, *_):
         finalizing_step(builder=self.builder, meta=self.meta, oauth=self.oauth)
 
+    @patch('eduvpn.manager.monitor_vpn')
     @patch('eduvpn.steps.finalize.store_provider')
-    def test_background(self, *_):
-        _background(builder=self.builder, dialog=self.dialog, meta=self.meta, oauth=self.oauth)
+    def test_background(self, store_provider, y):
+        store_provider.method.return_value = "blabla"
+        store_provider.return_value = "blabla"
+        try:
+            _background(builder=self.builder, dialog=self.dialog, meta=self.meta, oauth=self.oauth)
+        except DBusException as e:
+            pass
+
 
