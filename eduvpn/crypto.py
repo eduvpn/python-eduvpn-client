c9dcc0c002e6b822a74d6ec913d658e4eca0fa93
Wessel Peters <36157555+wjhpeters@users.noreply.github.com>
2019-10-09 11:49:35 +0200

    added the argument docs again.

--- eduvpn/crypto.py
+++ eduvpn/crypto.py
@@ -14,13 +14,27 @@ from cryptography.hazmat.backends import default_backend
 
 
 def common_name_from_cert(pem_data):  # type: (bytes) -> str
-    """Extract common name from client certificate."""
+    """
+    Extract common name from client certificate.
+
+    args:
+        pem_data (str): PEM encoded certificate
+    returns:
+        str: the common name of the client certificate.
+    """
     cert = x509.load_pem_x509_certificate(pem_data, default_backend())
     return cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
 
 
 def gen_code_verifier(length=128):  # type: (int) -> str
-    """Generate a high entropy code verifier, used for PKCE."""
+    """
+    Generate a high entropy code verifier, used for PKCE.
+
+    args:
+        length (int): length of the code
+    returns:
+        str:
+    """
     choices = 'abcdefghijklmnopqrstuvwxyz' \
               'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~'
     r = random.SystemRandom()
@@ -35,12 +49,24 @@ def gen_base32(length=20):  # type: (int) -> str
 
 
 def gen_code_challenge(code_verifier):  # type: (str) -> bytes
-    """Transform the PKCE code verifier in a code challenge."""
+    """
+    Transform the PKCE code verifier in a code challenge.
+
+    args:
+        code_verifier (str): a string generated with `gen_code_verifier()`
+    """
     sha256 = hashlib.sha256(code_verifier.encode())
     encoded = base64.urlsafe_b64encode(sha256.digest())
     return encoded.rstrip(b'=')
 
 
 def make_verifier(key):  # type: (str) -> nacl.signing.VerifyKey
-    """Create a NaCL verifier."""
+    """
+    Create a NaCL verifier.
+
+    args:
+        key (str): A verification key
+    returns:
+        nacl.signing.VerifyKey: a nacl verifykey object
+    """
     return nacl.signing.VerifyKey(key, encoder=nacl.encoding.Base64Encoder)
