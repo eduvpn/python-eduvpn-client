79ab41d3db0d61fc8419862d042d11568e002cbe
Aschwin Marsman <a.marsman@aynik.com>
2021-01-15 15:59:15 +0100

    Add function to retrieve a flag file name based on country code

--- eduvpn/ui/utils.py
+++ eduvpn/ui/utils.py
@@ -1,11 +1,17 @@
-from eduvpn.utils import logger
+import os
+
+from typing import Optional
+
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, GdkPixbuf
+
+from eduvpn.utils import logger
+from eduvpn.settings import FLAG_PREFIX
 
 
 # ui thread
-def error_helper(parent: Gtk.GObject, msg_big: str, msg_small: str) -> None:  # type: ignore
+def error_helper(parent: Gtk.Widget, msg_big: str, msg_small: str) -> None:  # type: ignore
     """
     Shows a GTK error message dialog.
     args:
@@ -18,3 +24,13 @@ def error_helper(parent: Gtk.GObject, msg_big: str, msg_small: str) -> None:  # 
     error_dialog.format_secondary_text(str(msg_small))  # type: ignore
     error_dialog.run()  # type: ignore
     error_dialog.hide()  # type: ignore
+
+
+def get_flag_image_file(country_code: str) -> Optional[str]:
+    logger.debug(f"get_flag_image: {country_code}")
+
+    if country_code:
+        flag_location = FLAG_PREFIX + country_code + "@1,5x.png"
+        if os.path.exists(flag_location):
+            return flag_location
+    return None
