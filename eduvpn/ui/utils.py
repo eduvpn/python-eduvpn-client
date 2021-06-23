85e1abc408489040f1f267091adcd66c499e2606
Alexander van Ratingen <470642+alvra@users.noreply.github.com>
2021-06-23 11:50:23 +0200

    Remove unused function.

--- eduvpn/ui/utils.py
+++ eduvpn/ui/utils.py
@@ -1,31 +1,6 @@
-from eduvpn.utils import logger
 import gi
 gi.require_version('Gtk', '3.0')  # noqa: E402
-from gi.repository import Gtk, GObject
-
-
-# ui thread
-def error_helper(parent: GObject,  # type: ignore
-                 msg_big: str,
-                 msg_small: str) -> None:
-    """
-    Shows a GTK error message dialog.
-    args:
-        parent (GObject): A GTK Window
-        msg_big (str): the big string
-        msg_small (str): the small string
-    """
-    logger.error(f"{msg_big}: {msg_small}")
-    error_dialog = Gtk.MessageDialog(  # type: ignore
-        parent,
-        0,
-        Gtk.MessageType.ERROR,  # type: ignore
-        Gtk.ButtonsType.OK,  # type: ignore
-        str(msg_big),
-    )
-    error_dialog.format_secondary_text(str(msg_small))  # type: ignore
-    error_dialog.run()  # type: ignore
-    error_dialog.hide()  # type: ignore
+from gi.repository import Gtk
 
 
 def show_ui_component(builder, component: str, show: bool):
