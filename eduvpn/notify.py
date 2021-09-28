b7c61b176f5fc1bfbf2bce7f4afd58b512dc53b7
Alexander van Ratingen <470642+alvra@users.noreply.github.com>
2021-09-28 17:38:26 +0200

    Show notification while connected.

--- eduvpn/notify.py
+++ eduvpn/notify.py
@@ -10,19 +10,23 @@ def initialize(app_variant: ApplicationVariant):
 
 class Notification:
     def __init__(self, app_variant: ApplicationVariant):
-        icon = GdkPixbuf.Pixbuf.new_from_file(app_variant.icon)  # type: ignore
-        self.notification = Notify.Notification.new(app_variant.name)
-        self.notification.set_icon_from_pixbuf(icon)
-        self.notification.set_app_name(app_variant.name)
-        self.is_shown = False
+        self.app_variant = app_variant
+        self.notification = None
+
+    def _build(self):
+        icon = GdkPixbuf.Pixbuf.new_from_file(self.app_variant.icon)  # type: ignore
+        notification = Notify.Notification.new(self.app_variant.name)
+        notification.set_icon_from_pixbuf(icon)
+        notification.set_app_name(self.app_variant.name)
+        return notification
 
     def show(self, title: str, message: str):
-        if not self.is_shown:
-            self.notification.update(title, message)
-            self.notification.show()
-            self.is_shown = True
+        if self.notification is None:
+            self.notification = self._build()
+        self.notification.update(title, message)
+        self.notification.show()
 
     def hide(self):
-        if self.is_shown:
+        if self.notification is not None:
             self.notification.close()
-            self.is_shown = False
+            self.notification = None
