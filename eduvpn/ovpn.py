ccc7319212e2c437d0ae78c8e35f75932b506f42
Alexander van Ratingen <470642+alvra@users.noreply.github.com>
2021-11-24 11:39:07 +0100

    Fix issue #418: check if any remotes remain when forcing tcp

--- eduvpn/ovpn.py
+++ eduvpn/ovpn.py
@@ -1,24 +1,132 @@
-import re
+from io import StringIO
+from typing import Optional, Callable, Iterable, List
+from gettext import gettext as _
 
 
-# matches for example: remote vpn.example.com 1234 udp
-# NOTE: multiline-mode does not consider \r
-#       so we must match for that explicitly.
-FIND_REMOTE_UDP_LINES = re.compile(
-    r'^(remote +\S+ +[0-9]+ +udp\r?)$',
-    re.MULTILINE
-)
+class Item:
+    def to_string(self) -> str:
+        raise NotImplementedError
+
+    def write(self, file):
+        line = self.to_string()
+        file.write(f'{line}\n')
+
+    def __eq__(self, other):
+        return self.__class__ is other.__class__ and self.__dict__ == other.__dict__
+
+    def __repr__(self):
+        fields = ','.join(f' {k}={v!r}' for k, v in self.__dict__.items())
+        return f'<{self.__class__.__name__}{fields}>'
 
 
-class Ovpn:
+class Field(Item):
+    def __init__(self, name: str, arguments: List[str]):
+        self.name = name
+        self.arguments = arguments
+
+    def to_string(self):
+        return f'{self.name} {" ".join(self.arguments)}'
+
+
+class Section(Item):
+    def __init__(self, tag: str, content: List[str]):
+        self.tag = tag
+        self.content = content
+
+    def to_string(self):
+        lines = []
+        lines.append(f'<{self.tag}>')
+        lines.extend(self.content)
+        lines.append(f'</{self.tag}>')
+        return '\n'.join(lines)
+
+
+class Comment(Item):
     def __init__(self, content: str):
         self.content = content
 
+    def to_string(self):
+        return f'#{self.content}'
+
+
+class Empty(Item):
+    def to_string(self):
+        return ''
+
+
+class InvalidOVPN(Exception):
+    pass
+
+
+def parse_ovpn(lines: Iterable[str]) -> Iterable[Item]:
+    current_section = None
+    for lineno, line in enumerate(lines):
+        if current_section is not None:
+            if line.strip() == f'</{current_section.tag}>':
+                yield current_section
+                current_section = None
+            else:
+                current_section.content.append(line)
+        elif line.startswith('#'):
+            yield Comment(line.rstrip()[1:])
+        elif line.startswith('<'):
+            line = line.rstrip()
+            assert line.endswith('>')
+            section = Section(line[1:-1], [])
+            current_section = section
+        elif line.rstrip() == '':
+            yield Empty()
+        else:
+            field_name, *arguments = line.rstrip().split()
+            yield Field(field_name, arguments)
+    assert current_section is None
+
+
+class Ovpn:
+    def __init__(self, content: List[Item]):
+        self.content = content
+
+    @classmethod
+    def parse(cls, content: str) -> 'Ovpn':
+        return cls(list(parse_ovpn(content.splitlines())))
+
     def write(self, file):
-        file.write(self.content)
+        for item in self.content:
+            item.write(file)
+
+    def to_string(self) -> str:
+        file = StringIO()
+        self.write(file)
+        return file.getvalue()
+
+    def contains_field(self, field_name: str) -> bool:
+        for item in self.content:
+            if isinstance(item, Field):
+                if item.name == field_name:
+                    return True
+        return False
+
+    def replace_fields(self, replace: Callable[[Field], Optional[Item]]):
+        new_content = []
+        for item in self.content:
+            if isinstance(item, Field):
+                replacement = replace(item)
+                if replacement is not None:
+                    new_content.append(replacement)
+            else:
+                new_content.append(item)
+        self.content = new_content
 
     def force_tcp(self):
-        self.content = FIND_REMOTE_UDP_LINES.sub(
-            r'# omitted to force tcp: \1',
-            self.content,
-        )
+        def replacement(field):
+            if field.name == 'remote' and field.arguments[2] == 'udp':
+                return Comment(f' omitted to force tcp: {field.to_string()}')
+            else:
+                return field
+
+        self.replace_fields(replacement)
+        if not self.contains_field('remote'):
+            raise InvalidOVPN(_(
+                "This connection does not provide TCP connections. "
+                "Disable the setting 'force-tcp' to use it."
+            ))
--- tests/test_ovpn.py
+++ tests/test_ovpn.py
@@ -1,26 +1,53 @@
 from unittest import TestCase
-from eduvpn.ovpn import Ovpn
+from eduvpn.ovpn import InvalidOVPN, Ovpn, Field, Section, Comment, Empty
 
 
 class OVPNTests(TestCase):
+    def test_parse_and_write(self):
+        ovpn_file = (
+            '# hello world\n'
+            'set a 1\n'
+            '\n'
+            '<ca>\n'
+            'the\n'
+            'certificate\n'
+            'content\n'
+            '</ca>\n'
+        )
+        ovpn = Ovpn.parse(ovpn_file)
+        self.assertEqual(ovpn.content, [
+            Comment(' hello world'),
+            Field('set', ['a', '1']),
+            Empty(),
+            Section('ca', ['the', 'certificate', 'content']),
+        ])
+        self.assertEqual(ovpn.to_string(), ovpn_file)
+
     def test_force_tcp(self):
-        ovpn = Ovpn(
+        ovpn = Ovpn.parse(
             'a\n'
             'remote vpn.example.com 1234 udp\n'
             'remote vpn.example.com 5678 udp\r\n'
             'remote vpn.example.com 1234 tcp\r'
-            'remote vpn.example.com 5678 tcp\rn'
+            'remote vpn.example.com 5678 tcp\r\n'
             'b\r\n'
             'c\n'
         )
         expected_content = (
-            'a\n'
+            'a \n'
             '# omitted to force tcp: remote vpn.example.com 1234 udp\n'
-            '# omitted to force tcp: remote vpn.example.com 5678 udp\r\n'
-            'remote vpn.example.com 1234 tcp\r'
-            'remote vpn.example.com 5678 tcp\rn'
-            'b\r\n'
-            'c\n'
+            '# omitted to force tcp: remote vpn.example.com 5678 udp\n'
+            'remote vpn.example.com 1234 tcp\n'
+            'remote vpn.example.com 5678 tcp\n'
+            'b \n'
+            'c \n'
         )
         ovpn.force_tcp()
-        self.assertEqual(ovpn.content, expected_content)
+        self.assertEqual(ovpn.to_string(), expected_content)
+
+    def test_force_tcp_fail(self):
+        ovpn = Ovpn.parse(
+            'remote vpn.example.com 1234 udp\n'
+        )
+        with self.assertRaises(InvalidOVPN):
+            ovpn.force_tcp()
