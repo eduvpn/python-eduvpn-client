35bb6cf33b5ef2da6331fe17df083b889d6e424e
Gijs Molenaar <gijs@pythonic.nl>
2022-04-22 15:53:08 +0200

    Resolve issue #447

--- eduvpn/__init__.py
+++ eduvpn/__init__.py
@@ -1,6 +1,6 @@
 import pkg_resources
 
 try:
-    __version__: str = pkg_resources.require("eduvpn")[0].version
+    __version__: str = pkg_resources.require("eduvpn-client")[0].version
 except pkg_resources.DistributionNotFound:
     __version__ = "0.0dev"
