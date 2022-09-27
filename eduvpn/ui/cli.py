158eebddc8d1d6ed52a16dff806818a2dfc6ebc7
jwijenbergh <jeroenwijenbergh@protonmail.com>
2022-09-27 17:00:53 +0200

    CLI: Connect in background to handle OAuth cancel

--- eduvpn/ui/cli.py
+++ eduvpn/ui/cli.py
@@ -100,7 +100,10 @@ class CommandLine:
     def connect_server(self, server):
         def connect(callback=None):
             try:
-                self.app.model.connect(server, callback)
+                @run_in_background_thread('connect')
+                def connect_background():
+                    self.app.model.connect(server, callback)
+                connect_background()
             except Exception as e:
                 print("Error connecting:", e, file=sys.stderr)
                 if callback:
