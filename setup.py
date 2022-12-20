9933efd2c909678d2aa6dbae53cfb5e7da37469c
jwijenbergh <jeroenwijenbergh@protonmail.com>
2022-12-20 21:18:55 +0100

    Setup: Bump to 3.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 from setuptools import setup, find_packages
 
-__version__ = "3.1.0"
+__version__ = "3.2.0"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
