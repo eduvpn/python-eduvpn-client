2185f02a94670cd0d5cc94ab1f34c1ef6bf25677
jwijenbergh <jeroenwijenbergh@protonmail.com>
2024-03-11 11:27:41 +0100

    Version: Update to 4.2.99.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 from setuptools import find_packages, setup
 
-__version__ = "4.2.1"
+__version__ = "4.2.99.0"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
