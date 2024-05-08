cf20f1ba7dae080816f0303ab080437b07b31981
jwijenbergh <jeroenwijenbergh@protonmail.com>
2024-05-08 13:22:57 +0200

    add search using common

--- eduvpn/discovery.py
+++ eduvpn/discovery.py
@@ -10,18 +10,15 @@ class DiscoOrganization:
     """The class that represents an organization from discovery
     :param: display_name: Dict[str, str]: The display name of the organizations
     :param: org_id: str: The organization ID
-    :param: keywords: Dict[str, str]: The dictionary of strings that the users gets to search on to find the server
     """
 
     def __init__(
         self,
         display_name: Dict[str, str],
         org_id: str,
-        keywords: Dict[str, str],
     ):
         self.display_name = display_name
         self.org_id = org_id
-        self.keywords = keywords
 
     @property
     def identifier(self):
@@ -39,7 +36,6 @@ class DiscoServer:
     """The class that represents a discovery server, this can be an institute access or secure internet server
     :param: base_url: str: The base URL of the server
     :param: display_name: Dict[str, str]: The display name of the server
-    :param: keywords: Dict[str, str]: The dictionary of keywords that the user can use to find the server
     :param: server_type: str: The server type as a string
     """
 
@@ -47,12 +43,10 @@ class DiscoServer:
         self,
         base_url: str,
         display_name: Dict[str, str],
-        keywords: Dict[str, str],
         server_type: str,
     ):
         self.base_url = base_url
         self.display_name = display_name
-        self.keywords = keywords
         self.server_type = server_type
 
     @property
@@ -70,18 +64,15 @@ class DiscoServer:
 def parse_disco_server(s: dict) -> DiscoServer:
     b_url = s["base_url"]
     display_name = s.get("display_name", "")
-    # display names are just space separated in server_list.json
-    keywords = s.get("keyword_list", {"en": ""})
     # Mandatory
     server_type = s["server_type"]
-    return DiscoServer(b_url, display_name, keywords, server_type)
+    return DiscoServer(b_url, display_name, server_type)
 
 
 def parse_disco_organization(o: dict) -> DiscoOrganization:
     display_name = o.get("display_name", "")
     org_id = o["org_id"]
-    keywords = o.get("keyword_list", {"en": ""})
-    return DiscoOrganization(display_name, org_id, keywords)
+    return DiscoOrganization(display_name, org_id)
 
 
 def parse_disco_servers(json_str: str) -> List[DiscoServer]:
